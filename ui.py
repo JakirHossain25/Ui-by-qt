5ba1caa8875b643cbd8192f92d6c470511e5ce26
JakirHossain25 <sarkerjakariya5@gmail.com>
2026-01-25 23:57:10 +0600

    2d mothord push to button

--- ui.py
+++ ui.py
@@ -3,14 +3,28 @@
 from PySide6.QtWidgets import QApplication ,QMainWindow,QPushButton
 import sys
 
+# app=QApplication(sys.argv)
+# window = QMainWindow()
+# window.setWindowTitle("This is a fist windo")
+
+# Button = QPushButton()
+# Button.setText( "push Me")
+
+# window.setCentralWidget(Button)
+
+# window.show()
+# app.exec()
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button hold app")
+
+        button = QPushButton("press Me!")
+        self.setCentralWidget(button)
+        
 app=QApplication(sys.argv)
-window = QMainWindow()
-window.setWindowTitle("This is a fist windo")
 
-Button = QPushButton()
-Button.setText( "push Me")
-
-window.setCentralWidget(Button)
-
+window= ButtonHolder()
 window.show()
 app.exec()
